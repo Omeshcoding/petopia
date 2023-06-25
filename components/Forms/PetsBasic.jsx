a4f9ebc5b5569781b2c94ec83744c6ff1cedea4d
umesh <umesharma.us11@gmail.com>
2023-06-25 22:41:13 +0530

    Created a custom hook for the sign up form for user and pet info

---
+++ components/Forms/PetsBasic.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const PetsBasics = () => {
+  return (
+    <form>
+      <div>
+        <label>First Name</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>Last Name</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>Phone</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label>Phone alt</label>
+        <input type="text" />
+      </div>
+    </form>
+  );
+};
+
+export default PetsBasics;
