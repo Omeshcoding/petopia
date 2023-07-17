4a440a6c757ca84247211aaaab0f8e63f8318b39
umesh <umesharma.us11@gmail.com>
2023-07-17 15:15:43 +0530

    Build the Payment and PaymentConfirm card, completed the styling

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,11 +1,14 @@
 import BookingPlans from '@components/BookingForm/BookingPlans';
 import BookingTime from '@components/BookingForm/BookingTime';
+import Payment from '@components/BookingForm/Payment';
+import PaymentConfirm from '@components/BookingForm/PaymentConfirm';
+
 import React from 'react';
 
 const Booking = () => {
   return (
     <div>
-      <BookingTime />
+      <PaymentConfirm />
     </div>
   );
 };
--- components/BookingForm/BookingTime.jsx
+++ components/BookingForm/BookingTime.jsx
@@ -71,38 +71,6 @@ const BookingTime = () => {
                 placeholder="Route preferences,leash location, treats given, etc."
               ></textarea>
             </div>
-            {/* 
-            </div>
-            <div className="flex justify-between w-[100%] ">
-              <div className="flex flex-col mx-auto gap-4">
-                <label>Phone </label>
-                <input
-                  type="text"
-                  className={`border-2 pl-5 border-lightBrown rounded-md py-4 pr-20 `}
-                  // value={phone}
-                  // onChange={(e) => handleChange({ phone: e.target.value })}
-                />
-              </div>
-              <div className="flex flex-col mx-auto gap-4">
-                <label>Phone Alt </label>
-                <input
-                  type="text"
-                  className={`border-2 pl-5 border-lightBrown rounded-md py-4 pr-20 `}
-                  // value={phoneAlt}
-                  // onChange={(e) => handleChange({ phoneAlt: e.target.value })}
-                />
-              </div>
-            </div>
-            <div className="flex justify-between w-[100%] ml-4 ">
-              <div className="flex flex-col mx-auto gap-4">
-                <label>City </label>
-                <input
-                  type="text"
-                  className={`border-2 pl-5 border-lightBrown rounded-md py-4 pr-20 `}
-                  // value={city}
-                  // onChange={(e) => handleChange({ city: e.target.value })}
-                />
-              </div> */}
           </div>
         </div>
       </div>
